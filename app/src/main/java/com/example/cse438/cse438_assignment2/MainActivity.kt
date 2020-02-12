1489ddda211141f72d8975122da6caf2ca55dfa2
Doug Shook <dsshook@gmail.com>
2020-02-12 16:29:16 -0600

    starter code

---
+++ app/src/main/java/com/example/cse438/cse438_assignment2/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cse438.cse438_assignment2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
